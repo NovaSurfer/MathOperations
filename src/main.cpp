daf37016f0d18022cd750a2c0a59f328a51149da
Maksim Ruts <tribar.ge@gmail.com>
2020-02-24 10:42:02 +0300

    Adds pow2 splitter, operation picker

--- src/main.cpp
+++ src/main.cpp
@@ -8,31 +8,33 @@ int main()
     // some testing..
     // TODO: add doctest for unit-testing
     Operations opers = Operations::MINUS | Operations::PLUS;
-//    opers &= ~Operations::PLUS;
+    //    opers &= ~Operations::PLUS;
 
     OperWeightMap weights {
         {Operations::PLUS, 10},  {Operations::MINUS, 9}, {Operations::MULTIPLY, 8},
         {Operations::DIVIDE, 7}, {Operations::POW, 6},   {Operations::SQRT, 5},
         {Operations::MOD, 4},    {Operations::LOG, 3},   {opers, 88}};
 
-
+    GeneratorRange<9, 25> range;
+    OperationPair op_pair[OPERATIONS_SIZE] {
+        {Operations::PLUS, 10},  {Operations::MINUS, 9}, {Operations::MULTIPLY, 8},
+        {Operations::DIVIDE, 7}, {Operations::POW, 6},   {Operations::SQRT, 5},
+        {Operations::MOD, 4},    {Operations::LOG, 3},
+    };
 
     GeneratorData gendata_lvl1;
-    GeneratorRange<9,25> range;
     gendata_lvl1.min_value = range.min;
     gendata_lvl1.max_value = range.max;
+    gendata_lvl1.weights = weights;
     const int sqrt_size_buff = 0;
+    const int pows2_size_buff = 0;
     constexpr size_t rng_size = range.size();
     constexpr size_t sqrs_size = range.squares_size(sqrt_size_buff);
-    Generator<rng_size, sqrs_size> generator_lvl1;
-    generator_lvl1.init(gendata_lvl1);
-
-//    GeneratorData gendata_lvl2;
-//    GeneratorRange<64,128> range2;
-//    gendata_lvl2.min_value = range2.min;
-//    gendata_lvl2.max_value = range2.max;
-//    Generator<range2.size()> generator_lvl2;
-//    generator_lvl2.init(gendata_lvl2);
+    constexpr size_t pows2_size = range.powsof2_size(pows2_size_buff);
+//    constexpr size_t oper_pairs_size = gendata_lvl1.oper_pairs_size();
+    constexpr static size_t lvl1_args[3] = {rng_size, sqrs_size, pows2_size};
+    Generator<lvl1_args> sdfs;
+    sdfs.init(gendata_lvl1);
 
     printf("Operation type: %d\nSome weight: %d", static_cast<unsigned>(opers), weights[opers]);
     return 0;
--- src/operation_generator.h
+++ src/operation_generator.h
@@ -19,6 +19,11 @@ namespace mathgens
             const double root = sqrt(n);
             return ((root - floor(root)) == 0.0);
         }
+
+        inline static constexpr bool is_power_of_two(unsigned n)
+        {
+            return (n != 0) && ((n & (n - 1)) == 0);
+        }
     }
 
     enum class Operations : unsigned
@@ -33,6 +38,8 @@ namespace mathgens
         MOD = (1u << 6u),
         LOG = (1u << 7u)
     };
+
+    constexpr unsigned OPERATIONS_SIZE = 8;
 
     using OperWeightMap = std::unordered_map<Operations, unsigned>;
 
@@ -77,12 +84,6 @@ namespace mathgens
         return static_cast<Operations>(~static_cast<unsigned>(oper));
     }
 
-    struct OperationData
-    {
-        Operations type;
-        unsigned number;
-    };
-
     template <size_t MIN, size_t MAX>
     struct GeneratorRange
     {
@@ -100,7 +101,7 @@ namespace mathgens
         /*
          * Returns number of perfect squares between [min, max]
          */
-        constexpr unsigned squares_size(size_t sqrts_size)
+        constexpr size_t squares_size(size_t sqrts_size) const
         {
             for(size_t i = MIN; i <= MAX; ++i) {
                 for(size_t j = 1; j * j <= i; ++j) {
@@ -110,66 +111,91 @@ namespace mathgens
             }
             return sqrts_size;
         }
+
+        constexpr size_t powsof2_size(size_t powsof2_size) const
+        {
+            for(size_t i = MIN; i <= MAX; ++i) {
+                if(utils::is_power_of_two(i))
+                    powsof2_size++;
+            }
+            return powsof2_size;
+        }
     };
+
+    struct OperationData
+    {
+        Operations type;
+        unsigned number;
+    };
+
+    // Struct for pair [operation, weight]
+    using OperationPair = OperationData;
 
     struct GeneratorData
     {
-        Operations operations;
         OperWeightMap weights;
         unsigned max_value;
         unsigned min_value;
     };
 
-    struct Answers
+    struct GenRes
     {
-        Operations operation;
-        unsigned number;
-    };
-
-    struct GeneratorResult
-    {
+        static constexpr size_t CORRECT_INDEX = 0;
         // For now hardcoded to 3;
-        Answers answers[3];
+        OperationData answers[3];
         unsigned result;
     };
 
-    template <size_t N, size_t N_SQRTS>
+    enum gen_data_args
+    {
+        N,
+        N_SQRTS,
+        N_POWS2,
+        THE_END
+    };
+
+    template <const size_t* SIZE>
     class Generator
     {
+        using arg = gen_data_args;
+
     public:
         void init(const GeneratorData& gendata);
         unsigned get_next_num();
         OperationData get_next_operation();
-        GeneratorResult next();
+        GenRes next();
 
     private:
         void split_numbers();
+        Operations pick_operation();
 
         GeneratorData gendata;
         unsigned current_value = 0;
         size_t id = 0;
-        unsigned numbs_array[N];
-        unsigned numbs_sqrts_array[N_SQRTS];
+        unsigned numbs_array[SIZE[arg::N]];
+        unsigned numbs_sqrts_array[SIZE[arg::N_SQRTS]];
+        unsigned numbs_pow2_array[SIZE[arg::N_POWS2]];
     };
 
-    template <size_t N, size_t N_SQRTS>
-    void Generator<N, N_SQRTS>::init(const GeneratorData& gendata)
+    template <const size_t* SIZE>
+    void Generator<SIZE>::init(const GeneratorData& gendata)
     {
         this->gendata = gendata;
+        constexpr size_t lenght = SIZE[arg::N];
 
-        if constexpr(N < 1)
+        if constexpr(lenght < 1)
             return;
 
         // Fill array
         unsigned min = gendata.min_value;
-        for(size_t i = 0; i < N; ++i) {
+        for(size_t i = 0; i < SIZE[arg::N]; ++i) {
             numbs_array[i] = min++;
         }
 
         // Shuffle array
         srand(time(nullptr));
-        for(size_t i = 0; i < N - 1; ++i) {
-            size_t j = i + rand() / (RAND_MAX / (N - i) + 1);
+        for(size_t i = 0; i < lenght - 1; ++i) {
+            size_t j = i + rand() / (RAND_MAX / (lenght - i) + 1);
             int t = numbs_array[j];
             numbs_array[j] = numbs_array[i];
             numbs_array[i] = t;
@@ -178,33 +204,58 @@ namespace mathgens
         split_numbers();
     }
 
-    template <size_t N, size_t N_SQRTS>
-    unsigned Generator<N, N_SQRTS>::get_next_num()
+    template <const size_t* SIZE>
+    unsigned Generator<SIZE>::get_next_num()
     {
-        unsigned result = numbs_array[id % N];
+        unsigned result = numbs_array[id % SIZE[arg::N]];
         id++;
         return result;
     }
 
-    template <size_t N, size_t N_SQRTS>
-    OperationData Generator<N, N_SQRTS>::get_next_operation()
+    template <const size_t* SIZE>
+    OperationData Generator<SIZE>::get_next_operation()
     {
         return OperationData();
     }
 
-    template <size_t N, size_t N_SQRTS>
-    GeneratorResult Generator<N, N_SQRTS>::next()
+    template <const size_t* SIZE>
+    GenRes Generator<SIZE>::next()
     {
-        unsigned result = get_next_num();
+        GenRes genres;
+        static size_t regular_id;
+        static size_t sqrts_id;
+        static size_t pows2_id;
+
+        Operations oper = pick_operation();
+
+        switch(oper) {
+        case Operations::PLUS:
+        case Operations::MINUS:
+        case Operations::MULTIPLY:
+        case Operations::DIVIDE:
+            genres.result = numbs_array[regular_id % SIZE[arg::N]];
+            break;
+        case Operations::POW:
+            genres.result = numbs_pow2_array[regular_id % SIZE[arg::N_POWS2]];
+            break;
+        case Operations::SQRT:
+            genres.result = numbs_sqrts_array[regular_id % SIZE[arg::N_SQRTS]];
+            break;
+        case Operations::MOD:
+            break;
+        case Operations::LOG:
+            break;
+        }
     }
 
-    template <size_t N, size_t N_SQRTS>
-    void Generator<N, N_SQRTS>::split_numbers()
+    template <const size_t* SIZE>
+    void Generator<SIZE>::split_numbers()
     {
+        constexpr size_t length = SIZE[arg::N];
         // Split perfect squares
         {
             size_t i = 0;
-            for(size_t j = 0; j < N; ++j) {
+            for(size_t j = 0; j < length; ++j) {
                 unsigned value = numbs_array[j];
                 if(utils::is_perfect_square(value)) {
                     numbs_sqrts_array[i] = value;
@@ -212,8 +263,35 @@ namespace mathgens
                 }
             }
         }
-        // TODO: Split numbers with power of 2
+        // Split numbers that are power of 2
+        {
+            size_t i = 0;
+            for(size_t j = 0; j < length; ++j) {
+                unsigned value = numbs_array[j];
+                if(utils::is_power_of_two(value)) {
+                    numbs_pow2_array[i] = value;
+                    ++i;
+                }
+            }
+        }
         // TODO: Split other arrays
+    }
+    template <const size_t* SIZE>
+    Operations Generator<SIZE>::pick_operation()
+    {
+        size_t sum_of_weight = 0;
+
+        for(const auto [key, val] : gendata.weights) {
+            sum_of_weight += val;
+        }
+
+        srand(time(nullptr));
+        size_t rnd = rand() % sum_of_weight;
+        for(const auto [key, val] : gendata.weights) {
+            if(rnd < val)
+                return key;
+            rnd -= val;
+        }
     }
 }
 #endif // MATHOPERATIONS_OPERATION_GENERATOR_H
